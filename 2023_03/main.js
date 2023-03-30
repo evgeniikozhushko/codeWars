45d238e3ae0239af3fa9258e34b4ba7e80e34d74
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-03-29 22:31:34 -0600

    Quick update

--- 2023_03/main.js
+++ 2023_03/main.js
@@ -42,14 +42,14 @@ function repeatStr (number, string) {
 
 // Test case
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
 
-// describe("Tests", function() {
-//     it ("Basic tests", function() {
-//     assert.strictEqual(repeatStr(3, "*"), "***");
-//     assert.strictEqual(repeatStr(5, "#"), "#####");
-//     assert.strictEqual(repeatStr(2, "ha "), "ha ha ");
-//     });
-// });
+describe("Tests", function() {
+    it ("Basic tests", function() {
+    assert.strictEqual(repeatStr(3, "*"), "***");
+    assert.strictEqual(repeatStr(5, "#"), "#####");
+    assert.strictEqual(repeatStr(2, "ha "), "ha ha ");
+    });
+});
