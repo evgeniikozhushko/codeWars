4b0370876ff4d336d690b6a6c1bfd824fe3fcfa0
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-10-08 09:43:06 -0600

    8kyu Rem 1st & last char

--- 2023_10/main.js
+++ 2023_10/main.js
@@ -63,3 +63,26 @@ function areYouPlayingBanjo(name) {
 //     assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
 //     })
 //   })
+
+// 3. 8 kyu / Remove First and Last Character. It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+    const newString = str.substring(1, str.length -1)
+    return newString
+    };
+
+// test
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("Fixed Tests", () => {
+//     assert.strictEqual(removeChar('eloquent'), 'loquen');
+//     assert.strictEqual(removeChar('country'), 'ountr');
+//     assert.strictEqual(removeChar('person'), 'erso');
+//     assert.strictEqual(removeChar('place'), 'lac');
+//     assert.strictEqual(removeChar('ooopsss'), 'oopss');
+//   });
+// });
