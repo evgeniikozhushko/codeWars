093c561ec65275c3b78f031111779acffb11db50
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-10-07 22:16:12 -0600

    October 7th Code Wars challenge

---
+++ 2023_10/main.js
@@ -0,0 +1,32 @@
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+function distinct(a) {
+    const newNonDuplicateArray = a.filter((item, index, self) => {
+        return self.indexOf(item) === index;
+    });
+    return newNonDuplicateArray;
+}
+console.log(distinct([1, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+
+// Sample Test
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Sample Tests", () => {
+//   it("test", () => {
+//     assert.deepEqual(distinct([1]), [1]);
+//     assert.deepEqual(distinct([1,2]), [1,2]);
+//     assert.deepEqual(distinct([1,1,2]), [1,2]);
+//   });
+// });
