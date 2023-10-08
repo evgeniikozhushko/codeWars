cef367023913fda15dd710e082428bbb7790a0c3
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-10-07 23:23:29 -0600

    Second CodeWar challenge

--- 2023_10/main.js
+++ 2023_10/main.js
@@ -1,4 +1,4 @@
-// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+// 1. Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
 
 // The order of the sequence has to stay the same.
 
@@ -30,3 +30,36 @@ console.log(distinct([1, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
 //     assert.deepEqual(distinct([1,1,2]), [1,2]);
 //   });
 // });
+
+// 2. Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+    if(name[0] === 'R' || name[0] === "r") {
+        return `${name} plays banjo`
+    }else {
+        return `${name} does not play banjo`
+    }
+}
+
+// test
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+//     assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+//     assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+//     assert.strictEqual(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+//     assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+//     })
+//   })
