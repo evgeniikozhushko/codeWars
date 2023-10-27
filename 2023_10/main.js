bc672144d22abb4c860f3b1eaf9c8a4b8bf7a562
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-10-26 22:01:45 -0600

    Another version to #12 kata

--- 2023_10/main.js
+++ 2023_10/main.js
@@ -293,16 +293,45 @@ function basicOp(operation, value1, value2) {
     }
 }
 
+// Best solution using switch case
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
+
+// Or
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+': return value1 + value2;
+        case '-': return value1 - value2;
+        case '*': return value1 * value2;
+        case '/': return value1 / value2;
+        default: return 'Operation must be one of + - * /';
+    }
+}
+
 // Sample test
 
 const Test = require('@codewars/test-compat');
 
 describe("Tests", () => {
-  it("test", () => {
+   it("test", () => {
 console.log("Basic tests\n");
 Test.assertSimilar(basicOp('+', 4, 7), 11);
 Test.assertSimilar(basicOp('-', 15, 18), -3);
 Test.assertSimilar(basicOp('*', 5, 5), 25);
 Test.assertSimilar(basicOp('/', 49, 7), 7);
-  });
+   });
 });
