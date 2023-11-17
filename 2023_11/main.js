ac9b8d96ed438fbc3604a5d581dbfc9616868ad3
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-11-17 08:00:24 -0700

    7 kyu kata

--- 2023_11/main.js
+++ 2023_11/main.js
@@ -20,4 +20,40 @@ describe("addBinary(1,2)", function() {
     });
 });
 
-// 
+// 7 kyu (do not understand)
+
+//Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+// Your function should only return a number, not the explanation about how you get that number.
+
+function getSum(a, b)
+{
+   if (a > b) {
+     [a,b] = [b, a]
+   }
+  const sum = ((b - a + 1) * (a + b)) / 2
+  return sum
+}
+
+// Sample Test
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {         
+    assert.strictEqual(getSum(0,-1), -1);
+    assert.strictEqual(getSum(0, 1),  1);
+    assert.strictEqual(getSum(2, 2),  2);
+  })
+});
