beee1864f2753319c85a061806c32a45bb9f1c97
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-11-16 20:25:14 -0700

    Sample test

--- 2023_11/main.js
+++ 2023_11/main.js
@@ -11,11 +11,13 @@ function addBinary(a,b) {
 const Test = require('@codewars/test-compat');
 
 describe("addBinary(1,2)", function() {
-  var results1 = addBinary(1,2);
-  it("Should return something that isn't falsy", function() {
+    var results1 = addBinary(1,2);
+    it("Should return something that isn't falsy", function() {
     Test.expect(results1, "Something is wrong, no results!");
-  });
-  it("Should return \"11\"", function() {
+    });
+    it("Should return \"11\"", function() {
     Test.assertEquals(results1, "11");
-  });
+    });
 });
+
+// 
