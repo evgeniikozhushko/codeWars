fe8a08f69e9a673be84d195dcb14ff35e4ab5b0c
evgeniikozhushko <climbersrock.eugene@gmail.com>
2024-12-13 21:25:50 -0700

    Kata 8 kyu Keep up the hoop

--- 2024_12/index.js
+++ 2024_12/index.js
@@ -36,3 +36,21 @@ describe("Tests", () => {
     assert.deepEqual(number(["a", "b", "c"]), ["1: a", "2: b", "3: c"], 'Return the correct line numbers');                
   });
 });
+
+
+// Kata 8 kyu Keep up the hoop
+
+function hoopCount(n) {
+  return n > 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+}
+
+// Sample test
+
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(hoopCount(3),"Keep at it until you get it" ) 
+Test.assertEquals(hoopCount(11),"Great, now move on to tricks" )
+  });
+});
