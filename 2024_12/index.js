a29dc7a521d2666c0d568a47daa7d06bebaf811e
evgeniikozhushko <climbersrock.eugene@gmail.com>
2024-12-28 00:18:57 -0700

    8 Kyu Say Hello

--- 2024_12/index.js
+++ 2024_12/index.js
@@ -347,3 +347,22 @@ function printAssignments(assignments) {
     console.log(`${giver} is giving a gift to ${receiver}`);
   }
 }
+
+// 8 Kyu Say Hello
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+
+// Sample test
+
+const { assert } = require('chai');
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(sayHello('Mr. Spock'), 'Hello, Mr. Spock')
+    assert.strictEqual(sayHello('Captain Kirk'), 'Hello, Captain Kirk')
+    assert.strictEqual(sayHello('Liutenant Uhura'), 'Hello, Liutenant Uhura')
+    assert.strictEqual(sayHello('Dr. McCoy'), 'Hello, Dr. McCoy')
+  });
+});
