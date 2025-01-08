439b46c475197ddb814ce271eaf4ee8e60fb03e9
evgeniikozhushko <climbersrock.eugene@gmail.com>
2025-01-07 23:59:17 -0700

    7kyu Lsist Filtering

--- 2025_01/index.js
+++ 2025_01/index.js
@@ -51,3 +51,20 @@ var debounce = function(fn, t) {
 * log('Hello'); // cancelled
 * log('Hello'); // Logged at t=100ms
 */
+
+// 7kyu Lsist Filtering - In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(l) {
+  return result = l.filter((item) => typeof item === 'number' && item >= 0);
+}
+
+// Sample Test Cases
+
+const assert = require('chai').assert;
+describe("Tests", () => {
+  it("test", () => {
+    assert.deepEqual(filter_list([1,2,'a','b']),[1,2], 'For input [1,2,"a","b"]');
+    assert.deepEqual(filter_list([1,'a','b',0,15]),[1,0,15], 'For input [1,"a","b",0,15]');
+    assert.deepEqual(filter_list([1,2,'aasf','1','123',123]),[1,2,123], 'For input [1,2,"aasf","1","123",123]');
+  });
+});
